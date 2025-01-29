acb863bde147b7637bb33fb65a5aec9619dc6057
rkouzmine <krrsea@gmail.com>
2025-01-29 10:11:25 +0300

    Модель данных - Post

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -66,13 +66,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(title, post.title)
-                && Objects.equals(link, post.link);
+        return Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link);
+        return Objects.hash(link);
     }
 
     @Override
