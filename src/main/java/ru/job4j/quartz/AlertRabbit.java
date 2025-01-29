287682b976f4dcd0a644dc39db1e07b02d0b77d6
rkouzmine <krrsea@gmail.com>
2025-01-30 01:23:10 +0300

    Quartz

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,18 +2,29 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AlertRabbit.class.getName());
+
     public static void main(String[] args) {
         try {
+            LOG.info("Starting scheduler...");
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(Integer.parseInt(loadProperties().getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -21,7 +32,7 @@ public class AlertRabbit {
                     .build();
             scheduler.scheduleJob(job, trigger);
         } catch (SchedulerException se) {
-            se.printStackTrace();
+            LOG.error("Execution schedule error.", se);
         }
     }
 
@@ -31,4 +42,16 @@ public class AlertRabbit {
             System.out.println("Rabbit runs here ...");
         }
     }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            LOG.error("Failed to load properties: {}", e.getMessage(), e);
+        }
+        return properties;
+    }
+
 }
